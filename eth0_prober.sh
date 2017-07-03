5059f05868a78e5377813c4016727fdfd3a59d41
snitopi <salvatore.nitopi@gmail.com>
2017-07-03 05:15:11 +0200

    Aggiunto prober

---
+++ eth0_prober.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+ifconfig eth0 up
+
+prober () {
+while [[ true ]]; do
+
+	plugged=$(cat /sys/class/net/eth0/carrier)
+	if [[ plugged == 1 ]]; then
+		
+		echo "[*] Plugged"
+
+		connected=$(ifconfig eth0 | grep "inet " | awk '{print $2}' | cut -d '.' -f2)
+		if [[ connected ]]; then
+			
+			echo "[*] Connected"
+
+		fi
+
+	fi
+
+	echo "LOL"
+	sleep 1
+done
+
+}
+
+prober
+
+#cat /sys/class/net/eth0/carrier
+#ifconfig eth0 | grep "inet " | awk '{print $2}' | cut -d '.' -f2
